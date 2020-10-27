4ff5ed22f10d0f6112032d2de340dd49c6d1fd66
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2020-10-27 18:03:46 -0500

    pipe date example

--- src/app/fecha-actual/fecha-actual.component.ts
+++ src/app/fecha-actual/fecha-actual.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component ({
   selector: 'app-fecha-actual',
   template: `
-    <p>Colombia, {{ hoy | date: 'd/M/y H:m'}} </p>
+    <!-- <p>Colombia, {{ hoy | date: 'd/M/y H:m'}} </p> -->
+    <!-- PIPE DATE EXAMPLE -->
+    <p>Colombia, {{ hoy | date: 'd-M-y'}} a las {{ hoy | date: 'H:m Z' }} </p>
     <app-copyright></app-copyright>
   `,
   styleUrls: ['./fecha-actual.component.scss']
